c18915e24ee572ad18f985441548305e15994b22
王强 <qiang.wang@dianping.com>
2015-01-21 10:58:39 +0800

    meishij is crawled.

--- dish_spider/spiders/meishij_spider.py
+++ dish_spider/spiders/meishij_spider.py
@@ -7,56 +7,30 @@ class MeishijSpider(CrawlSpider):
     name = "meishij"
     allowed_domains = ["meishij.net"]
     start_urls = [
-        'http://www.meishij.net/chufang/diy/jiangchangcaipu/',
-        'http://www.meishij.net/chufang/diy/haixian/',
-        'http://www.meishij.net/chufang/diy/tangbaocaipu/',
-        'http://www.meishij.net/chufang/diy/sushi/',
-        'http://www.meishij.net/chufang/diy/jiangliaozhanliao/',
-        'http://www.meishij.net/chufang/diy/huoguo/',
-        'http://www.meishij.net/chufang/diy/tianpindianxin/',
-        'http://www.meishij.net/chufang/diy/gaodianxiaochi/',
-        'http://www.meishij.net/chufang/diy/ganguo/',
-        'http://www.meishij.net/chufang/diy/rujiangcai/',
-        'http://www.meishij.net/chufang/diy/yinpin/',
+        'http://www.meishij.net/chufang/diy/guowaicaipu1/canqianxiaochi/',
+        'http://www.meishij.net/chufang/diy/guowaicaipu1/tangpin/',
+        'http://www.meishij.net/chufang/diy/guowaicaipu1/zhucai/',
+        'http://www.meishij.net/chufang/diy/guowaicaipu1/zhushi/',
+        'http://www.meishij.net/chufang/diy/guowaicaipu1/tiandian/',
     ]
 
-    liangcai_allow = ('chufang/diy/langcaipu/$', 'chufang/diy/langcaipu/.&page=[0-9]+',)
-    #liangcai_allow = ('chufang/diy/langcaipu/$',)
-    liangcai_deny = ('chufang/diy/langcaipu/[0-9]+.html',) 
-    haixian_allow = ('chufang/diy/haixian/$', 'chufang/diy/haixian/.&page=[0-9]+')
-    haixian_deny = ('chufang/diy/haixian/[0-9]+.html',) 
-    tangbao_allow = ('chufang/diy/tangbaocaipu/$', 'chufang/diy/tangbaocaipu/.&page=[0-9]+')
-    tangbao_deny = ('chufang/diy/tangbaocaipu/[0-9]+.html',) 
-    sushi_allow = ('chufang/diy/sushi/$', 'chufang/diy/sushi/.&page=[0-9]+')
-    sushi_deny = ('chufang/diy/sushi/[0-9]+.html',) 
-    jiangliao_allow = ('chufang/diy/jiangliaozhanliao/$', 'chufang/diy/jiangliaozhanliao/.&page=[0-9]+')
-    jiangliao_deny = ('chufang/diy/jiangliaozhanliao/[0-9]+.html',) 
-    huoguo_allow = ('chufang/diy/huoguo/$', 'chufang/diy/huoguo/.&page=[0-9]+')
-    huoguo_deny = ('chufang/diy/huoguo/[0-9]+.html',) 
-    tianpin_allow = ('chufang/diy/tianpindianxin/$', 'chufang/diy/tianpindianxin/.&page=[0-9]+')
-    tianpin_deny = ('chufang/diy/tianpindianxin/[0-9]+.html',)
-    gaodianxiaochi_allow = ('chufang/diy/gaodianxiaochi/$', 'chufang/diy/gaodianxiaochi/.&page=[0-9]+')
-    gaodianxiaochi_deny = ('chufang/diy/gaodianxiaochi/[0-9]+.html',)
-    ganguo_allow = ('chufang/diy/ganguo/$', 'chufang/diy/ganguo/.&page=[0-9]+')
-    ganguo_deny = ('chufang/diy/ganguo/[0-9]+.html',)
-    jianlu_allow = ('chufang/diy/rujiangcai/$', 'chufang/diy/rujiangcai/.&page=[0-9]+')
-    jianlu_deny = ('chufang/diy/rujiangcai/[0-9]+.html',)
-    yinpin_allow = ('chufang/diy/yinpin/$', 'chufang/diy/yinpin/.&page=[0-9]+')
-    yinpin_deny = ('chufang/diy/yinpin/[0-9]+.html',)
+    canqian_allow = ('chufang/diy/guowaicaipu1/canqianxiaochi/$', 'chufang/diy/guowaicaipu1/canqianxiaochi/.&page=[0-9]+')
+    canqian_deny = ('chufang/diy/guowaicaipu1/canqianxiaochi/[0-9]+.html',)
+    tangpin_allow = ('chufang/diy/guowaicaipu1/tangpin/$', 'chufang/diy/guowaicaipu1/tangpin/.&page=[0-9]+')
+    tangpin_deny = ('chufang/diy/guowaicaipu1/tangpin/[0-9]+.html',)
+    zhucai_allow = ('chufang/diy/guowaicaipu1/zhucai/$', 'chufang/diy/guowaicaipu1/zhucai/.&page=[0-9]+')
+    zhucai_deny = ('chufang/diy/guowaicaipu1/zhucai/[0-9]+.html',)
+    zhushi_allow = ('chufang/diy/guowaicaipu1/zhushi/$', 'chufang/diy/guowaicaipu1/zhushi/.&page=[0-9]+')
+    zhushi_deny = ('chufang/diy/guowaicaipu1/zhushi/[0-9]+.html',)
+    tiandian_allow = ('chufang/diy/guowaicaipu1/tiandian/$', 'chufang/diy/guowaicaipu1/tiandian/.&page=[0-9]+')
+    tiandian_deny = ('chufang/diy/guowaicaipu1/tiandian/[0-9]+.html',)
 
     rules = (
-        Rule(LinkExtractor(allow=liangcai_allow, deny= liangcai_deny), callback='parse_item', follow=True),
-        #Rule(LinkExtractor(allow=('chufang/diy/langcaipu/',), deny= liangcai_deny), callback='parse_item'),
-        Rule(LinkExtractor(allow=haixian_allow, deny= haixian_deny), callback='parse_item', follow=True),
-        Rule(LinkExtractor(allow=tangbao_allow, deny= susi_deny), callback='parse_item', follow=True),
-        Rule(LinkExtractor(allow=susi_allow, deny= tangbao_deny), callback='parse_item', follow=True),
-        Rule(LinkExtractor(allow=jiangliao_allow, deny= jiangliao_allow), callback='parse_item', follow=True),
-        Rule(LinkExtractor(allow=huoguo_allow, deny= huoguo_deny), callback='parse_item', follow=True),
-        Rule(LinkExtractor(allow=tianpin_allow, deny= tianpin_deny), callback='parse_item', follow=True),
-        Rule(LinkExtractor(allow=gaodianxiaochi_allow, deny= gaodianxiaochi_deny), callback='parse_item', follow=True),
-        Rule(LinkExtractor(allow=ganguo_allow, deny= ganguo_deny), callback='parse_item', follow=True),
-        Rule(LinkExtractor(allow=jianlu_allow, deny= jianlu_deny), callback='parse_item', follow=True),
-        Rule(LinkExtractor(allow=yinpin_allow, deny= yinpin_deny), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=canqian_allow, deny=canqian_deny), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=tangpin_allow, deny= tangpin_deny), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=zhucai_allow, deny= zhucai_deny), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=zhushi_allow, deny= zhushi_deny), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=tiandian_allow, deny= tiandian_deny), callback='parse_item', follow=True),
     )
 
     category_dict = dict()
@@ -72,6 +46,12 @@ class MeishijSpider(CrawlSpider):
     category_dict['ganguo'] = u'干果'
     category_dict['rujiangcai'] = u'酱卤'
     category_dict['yinpin'] = u'饮品'
+    category_dict['canqianxiaochi'] = u'餐前小菜'
+    category_dict['tangpin'] = u'汤品'
+    category_dict['zhucai'] = u'主菜'
+    category_dict['zhushi'] = u'主食'
+    category_dict['tiandian'] = u'甜点'
+
 
     def parse_item(self, response):
         i = response.url.find('?')
